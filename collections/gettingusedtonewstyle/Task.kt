9810a50e4081054be73c0bed5c85247cc7768052
tetuya-azami <zqianjian35@gmail.com>
2023-12-30 07:14:14 +0900

    collections

---
+++ collections/gettingusedtonewstyle/Task.kt
@@ -0,0 +1,10 @@
+package collections.gettingusedtonewstyle
+
+fun doSomethingWithCollection(collection: Collection<String>): Collection<String>? {
+
+    val groupsByLength = collection.groupBy { s -> s.length }
+
+    val maximumSizeOfGroup = groupsByLength.values.maxOfOrNull { group -> group.size }
+
+    return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
+}
