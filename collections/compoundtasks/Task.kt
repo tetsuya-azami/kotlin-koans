6c53730990681e2e4c4c31538e79ddf91c51f4cc
tetuya-azami <zqianjian35@gmail.com>
2023-12-29 09:41:29 +0900

    collections

---
+++ collections/compoundtasks/Task.kt
@@ -0,0 +1,16 @@
+package collections.compoundtasks
+
+// Find the most expensive product among all the delivered products
+// ordered by the customer. Use `Order.isDelivered` flag.
+fun findMostExpensiveProductBy(customer: Customer): Product? {
+    return customer.orders.filter { it.isDelivered }.flatMap(Order::products).maxByOrNull { it.price }
+}
+
+// Count the amount of times a product was ordered.
+// Note that a customer may order the same product several times.
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
+    val orderedProducts = customers.flatMap { it.getOrderedProducts() }
+    return orderedProducts.count { it == product }
+}
+
+fun Customer.getOrderedProducts(): List<Product> = orders.flatMap(Order::products)
---
+++ collections/foldandreduce/Task.kt
@@ -0,0 +1,7 @@
+package collections.foldandreduce
+
+// Return the set of products that were ordered by all customers
+fun Shop.getProductsOrderedByAll(): Set<Product> =
+    customers.map(Customer::getOrderedProducts).reduce { orderedByAll, product -> orderedByAll.intersect(product) }
+
+fun Customer.getOrderedProducts(): Set<Product> = orders.flatMap(Order::products).toSet()
